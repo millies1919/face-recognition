29bbe3b953619af276740dd5ade4ccb6fa2f5ff1
Matt Illies <mattillies19@gmail.com>
2019-08-12 17:12:15 -0500

    Added API call and button functionality
    user can now add image URL to input and it will appear on Detect is clicked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import Navigation from './components/navigation/navigation'
-import Logo from './components/logo/logo'
-import ImageLinkForm from './components/imagelinkform/imagelinkform'
-import Particles from 'react-particles-js'
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imagelinkform/imagelinkform';
+import FaceRecognition from './components/facerecognition/facerecognition.js'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '908fba15668c496eaae5bedfbc8553f1'
+ });
 
 const particlesOptions = {
   particles: {
@@ -17,7 +23,36 @@ const particlesOptions = {
       }
     }
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  onInputChange = (e) => {
+    this.setState({input: e.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render(){
   return (
     <div className="App">
       <Particles className="particles"
@@ -25,10 +60,11 @@ function App() {
       />
       <Navigation />
       <Logo />
-      <ImageLinkForm />
-       {/* <FaceRecognition />*/} 
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition imageUrl={this.state.imageUrl}/> 
     </div>
-  );
+    );
+  }
 }
 
 export default App;
