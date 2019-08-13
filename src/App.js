89c25870253aa450f0a3873849ab1d1029a4fb93
Matt Illies <mattillies19@gmail.com>
2019-08-12 19:40:34 -0500

    Face recognition works
    The application will box in a face in a given image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imagelinkform/imagelinkform';
 import FaceRecognition from './components/facerecognition/facerecognition.js'
+import SignIn from './components/signin/signin'
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
@@ -33,23 +34,35 @@ class App extends React.Component {
     }
   }
 
+  calculateFaceLocation = (data) => {
+      const findFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      console.log(findFace)
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: findFace.left_col * width,
+        topRow: findFace.top_row * height,
+        rightCol: width - (findFace.right_col * width),
+        bottomRow: height - (findFace.bottom_row * height),
+      }
+  }
+
+  displayFaceBox = (box) => {
+      this.setState({box: box});
+  }
+
   onInputChange = (e) => {
-    this.setState({input: e.target.value})
+    this.setState({input: e.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
+    this.setState({imageUrl: this.state.input});
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
 
   render(){
@@ -61,7 +74,7 @@ class App extends React.Component {
       <Navigation />
       <Logo />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecognition imageUrl={this.state.imageUrl}/> 
+      <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/> 
     </div>
     );
   }
