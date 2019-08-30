00459920d41bb56a210342a5cb1e4651e14550dd
Matt Illies <mattillies19@gmail.com>
2019-08-29 19:26:18 -0500

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -24,24 +24,24 @@ const particlesOptions = {
         }
       }
     }
-
+const initialUserState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends React.Component {
   constructor(){
     super()
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialUserState 
   }
 
   loadUser = (data) => {
@@ -94,6 +94,7 @@ class App extends React.Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
         })
+        .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -102,7 +103,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if(route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState({initialUserState})
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -34,6 +34,7 @@ class SignIn extends React.Component {
         this.props.onRouteChange('home');
       }
     })
+    .catch(console.log)
   }
 
   render() {
